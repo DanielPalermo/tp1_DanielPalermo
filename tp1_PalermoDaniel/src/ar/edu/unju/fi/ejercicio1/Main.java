8b1565c1b5e7d8b92cfc6f942e704d54a07178d6
danip <danip@DESKTOP-TTAUVM8>
2024-04-05 22:58:52 -0300

    Se agrega el paquete y la clase para el primer punto

---
+++ tp1_PalermoDaniel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float n1=8,n2=5,n3=1,n4=20,n5=9;
+		float suma = n1+n2+n3+n4+n5;
+		float promedio = suma / 5;
+		System.out.println("El promedio es:"+promedio);
+		
+	}
+
+}
