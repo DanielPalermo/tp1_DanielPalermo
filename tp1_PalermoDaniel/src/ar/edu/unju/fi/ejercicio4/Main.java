1d0234a78d8e10170dd3428e4bffa8052f94f9da
danip <danip@DESKTOP-TTAUVM8>
2024-04-09 00:45:28 -0300

    Se agregan correcciones al 4to punto

--- tp1_PalermoDaniel/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp1_PalermoDaniel/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -8,8 +8,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int fact, num;
+		do {
 		System.out.println("Ingres valor ");
 		num = sc.nextInt();
+		}while(num<=0 || num > 10);
 		fact = 1;
 		while(num > 0 ) {
 			fact= fact * num;
