1ec6260bef8c07248e307a34f81e5fb5825b5070
danip <danip@DESKTOP-TTAUVM8>
2024-04-09 00:17:52 -0300

    Agregado paquete y clase del segundo punto

---
+++ tp1_PalermoDaniel/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,32 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nomPais = "Argentina";
+		
+		System.out.println("Nombre del pais: " + nomPais);
+		
+		int edad = 20;
+		
+		System.out.println("Edad: " + edad);
+		
+		float alturaEdificio = 100;
+		
+		System.out.println(alturaEdificio);
+		
+		double precioProducto = 50.50;
+		
+		System.out.println("Precio del producto: " + precioProducto);
+		
+		long nroTelefono = 3884340480L;
+		
+		System.out.println("Numero de telefono: " + nroTelefono);
+		
+		double coseno = Math.cos(0.5);
+		
+		System.out.println("Coseno: " + coseno);
+		
+	}
+
+}
