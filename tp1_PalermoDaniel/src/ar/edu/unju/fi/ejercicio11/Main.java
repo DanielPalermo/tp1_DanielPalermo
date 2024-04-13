a80afb6cb5e82049e65e818bab67a312b0b8db31
danip <danip@DESKTOP-TTAUVM8>
2024-04-13 00:17:49 -0300

    Se agregan paquetes y clases del punto 11 de la continuacion

---
+++ tp1_PalermoDaniel/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j=40;
+		for(int i = 2; j > 0 ; i++){
+			System.out.println(i*j + " ");
+			j--;
+		}
+	}
+
+}
